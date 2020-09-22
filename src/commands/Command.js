60a395a74852a206e9bfe6556072d9bc628741eb
August <august@augu.dev>
2020-09-22 04:50:38 -0700

    a

--- src/commands/Command.js
+++ src/commands/Command.js
@@ -23,4 +23,40 @@
 /**
  * Represents a [Command] to execute when a user has typed out a command
  */
-module.exports = class Command {};
+module.exports = class Command {
+  /**
+   * Creates a new [Command] instance
+   * @param {CommandInfo} info The command's information
+   */
+  constructor(info) {
+    this.constructor._validate(info);
+
+    /**
+     * The command's description
+     * @type {string | ((client: import('./CommandClient')) => string)}
+     */
+    this.description = info.description;
+
+    /**
+     * List of inhibitors to run
+     * @type {string[]}
+     */
+    this.inhibitors = info.inhibitors || [];
+
+    /**
+     * List of aliases to set this command has
+     * @type {string[]}
+     */
+    this.aliases = info.aliases || [];
+  }
+};
+
+/**
+ * @typedef {object} CommandInfo
+ * @prop {string} name The command's name
+ * @prop {string | ((client: import('./CommandClient')) => string)} description The command's description
+ * @prop {string | ((command: Command) => string)} [usage=''] The command's usage
+ * @prop {string[]} [inhibitors=[]] List of inhibitors to run
+ * @prop {string[]} [aliases=[]] Any additional aliases to set this command as
+ * @prop {string} [category='none'] The category of the command, will default to `'none'`
+ */
