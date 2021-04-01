cfd0198366d87dfc9687eb93844bc02a50d2705d
Noel <cutie@floofy.dev>
2021-03-31 17:41:45 -0700

    debug info

--- src/models/Channel.ts
+++ src/models/Channel.ts
@@ -58,6 +58,7 @@ export class Channel extends Base<APIPartialChannel> {
       case 5: return new NewsChannel(client, data);
       case 6: return new StoreChannel(client, data);
       default:
+        client.debug(`Unknown Type [${data.type}]`, 'Channel type is not available on this version of Wumpcord you\'re running. Switch to indev branch or submit a PR?');
         return null;
     }
   }
